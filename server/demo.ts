f4535afaf4d4d4c53735cf5a5b0b03d9774d4f96
PhamTrung31 <117684484+PhamTrung31@users.noreply.github.com>
2025-10-02 16:39:54 +0700

    chore: import have js

--- server/demo.ts
+++ server/demo.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { handleDemo } from "./routes/demo";
+import { handleDemo } from "./routes/demo.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
