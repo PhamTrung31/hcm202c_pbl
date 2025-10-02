428b2690a4ef9703ede8f2b69d567c161fd3e733
PhamTrung31 <117684484+PhamTrung31@users.noreply.github.com>
2025-10-02 14:37:42 +0700

    chore: reinstall package-lock and change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,16 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { createServer } from "./server";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    fs: {
-      allow: [
-        path.resolve(__dirname),
-        path.resolve(__dirname, "client"),
-        path.resolve(__dirname, "shared"),
-      ],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
-  },
+export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "client-dist",
   },
-  plugins: [react(), ...(mode === "development" ? [expressPlugin()] : [])],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
       "@shared": path.resolve(__dirname, "./shared"),
     },
   },
-}));
-
-function expressPlugin(): Plugin {
-  return {
-    name: "express-plugin",
-    apply: "serve", // Only apply during development (serve mode)
-    configureServer(server) {
-      const app = createServer();
-
-      // Add Express app as middleware to Vite dev server
-      server.middlewares.use(app);
-    },
-  };
-}
+});
